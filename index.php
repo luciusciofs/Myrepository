c9da274c51ef3e25fba7cc7dff2ad59319d31a9c
luciusciofs <luciustoma@gmail.com>
2021-10-06 12:31:58 +0000

    commit tast

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
     <title><?php echo "Titolo della pagina"; ?></title>
 </head>
 <body>
-    Sono le ore <?php echo date('H:i'); ?> del giorno <?php echo date('d/m/Y'); ?>.
+    Sono le ore <?php echo date('H:i'); ?> del giorno oggi <?php echo date('d/m/Y'); ?>.
 </body>
 </html>
