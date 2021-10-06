8d48ab1d53309b2ad8589242b729046694bd00eb
luciusciofs <luciustoma@gmail.com>
2021-10-06 13:01:06 +0000

    prova

--- www/index.php
+++ www/index.php
@@ -4,6 +4,6 @@
     <title><?php echo "Titolo della pagina"; ?></title>
 </head>
 <body>
-    Sono le ore <?php echo date('H:i'); ?> ciauuu del giorno domani <?php echo date('d/m/Y'); ?>.
+    Sono le ore <?php echo date('H:i'); ?> ciauuu ffh dgdgf del giorno domani <?php echo date('d/m/Y'); ?>.
 </body>
 </html>
